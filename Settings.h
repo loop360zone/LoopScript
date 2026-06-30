9356681267b4fbac20a4d4662b4425fe0b97ac13
loop360zone <loop360zone@gmail.com>
2026-06-30 11:09:40 +0300

    Add files via upload

--- Settings.h
+++ Settings.h
@@ -13,7 +13,6 @@ extern AtomicBool bIsAppActive;
 extern AtomicBool bShowMenu;
 extern AtomicBool Cheatoff;
 
-// ESP
 extern AtomicBool bEnableESP;
 extern AtomicBool bBoxESP;
 extern AtomicBool bLineESP;
@@ -27,7 +26,6 @@ extern AtomicInt lineOrigin;
 extern AtomicInt lineTarget;
 extern AtomicFloat lineThickness;
 
-// Aimbot
 extern AtomicBool bAimEnable;
 extern AtomicInt AimPart;
 extern AtomicBool bAimLine;
@@ -40,7 +38,6 @@ extern AtomicBool bCrosshair;
 extern AtomicBool FovEnable;
 extern AtomicFloat FovVal;
 
-// Movement
 extern AtomicBool bSpeedHack;
 extern AtomicBool bFlyEnable;
 extern AtomicFloat FlySpeed;
@@ -51,18 +48,15 @@ extern AtomicFloat Carspeed;
 extern AtomicBool bCarFly;
 extern AtomicFloat carFlySpeed;
 
-// Player scale
 extern AtomicBool ScaleEnable;
 extern AtomicBool BigHead;
 extern AtomicBool BigChest;
 extern AtomicBool BigBody;
 extern AtomicFloat ScaleVal;
 
-// Weapon
 extern AtomicBool bInstantReload;
 extern AtomicBool bFastShoot;
 
-// Misc
 extern AtomicBool bStreamerMode;
 extern AtomicBool bEnableNotifications;
 extern AtomicInt MenuTheme;
@@ -70,7 +64,6 @@ extern AtomicFloat uiScale;
 extern AtomicInt selectedTab;
 extern AtomicInt targetFPS;
 
-// Teleport
 extern AtomicBool bAutoTeleport;
 extern AtomicInt selectedTeleport;
 
